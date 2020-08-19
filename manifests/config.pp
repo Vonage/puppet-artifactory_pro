a062b622df51b36e3defc87ab45157dba630df7f
James Stremick <james.stremick@vonage.com>
2020-08-19 14:40:07 -0400

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -15,7 +15,7 @@ class artifactory_pro::config {
   }
 
   # Add the license file
-  file { "${::artifactory::artifactory_home}/etc/artifactory.lic":
+  file { "${::artifactory::artifactory_home}/etc/artifactory/artifactory.lic":
     ensure  => file,
     content => $::artifactory_pro::license_key,
     mode    => '0664',
